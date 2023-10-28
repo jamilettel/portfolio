5da488d2a139f618d4260aea784b5bbbdc6cc2bc
Jamil Ettel <jamil.ettel@epitech.eu>
2023-10-28 20:16:29 +0200

    :sparkles: Added animate in + per page animate in

--- src/components/animated-transitions/AnimatedContainer.tsx
+++ src/components/animated-transitions/AnimatedContainer.tsx
@@ -4,6 +4,7 @@ import { useTransitionContext } from "@/contexts/TransitionContext";
 import { getIdFromPathname } from "@/utils/animated-utils";
 import { usePathname } from "next/navigation";
 import "./AnimatedContainer.scss";
+import { useMemo } from "react";
 
 export default function AnimatedContainer({
   children,
@@ -14,22 +15,25 @@ export default function AnimatedContainer({
 }) {
   const { savedChildren, savedId, animateOutUnknown } = useTransitionContext();
   const pathname = usePathname();
+  const currentId = useMemo(() => getIdFromPathname(pathname), []);
 
   let finalClassname = className;
   if (animateOutUnknown) {
     finalClassname += ' animate-out animate-out-unknown';
+  } else if (savedId && savedId !== currentId) {
+    finalClassname += ` animate-in animate-in-${savedId}`;
   }
 
   return (
     <>
-      <main id={getIdFromPathname(pathname)} className={className}>
+      <main id={currentId} className={finalClassname}>
         {children}
       </main>
       {savedChildren && (
         <main
           dangerouslySetInnerHTML={{ __html: savedChildren.innerHTML }}
           id={savedId}
-          className={`${savedChildren.className ?? ""} animate-out`}
+          className={`${savedChildren.className ?? ""} animate-out animate-out-${currentId}`}
         />
       )}
     </>
