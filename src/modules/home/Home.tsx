31499c4e664806b688ec31bd83a563f4ab805cac
Jamil Ettel <jamil.ettel@epitech.eu>
2023-10-28 18:04:34 +0200

    :pencil2: fixed typo :iphone: responsive works now

--- src/modules/home/Home.tsx
+++ src/modules/home/Home.tsx
@@ -10,13 +10,12 @@ export default function Home() {
   return (
     <AnimatedContainer className="main-content">
       <div className="content">
-
         <h1>Hello!</h1>
         <p>Welcome to my portfolio! I am Jamil, a fullstack developer.</p>
-        <p>Now this website is still in development, but in the meantime you can checkout my <a href="https://www.github.com/jamilettel">Github</a> (my projects there are pretty dated by now).</p>
+        <p>Now this website is still in development, but in the meantime you can checkout my <a href="https://www.github.com/jamilettel">Github</a> (my projects there are pretty dated by now though).</p>
         <br />
         <p>And you can also take a look at these amazing transitions! <AnimatedLink href="/test">go to /test</AnimatedLink></p>
-        <p>This was developed only using NextJS, and it event retains state:</p>
+        <p>This was developed only using NextJS, and it even retains state:</p>
         <div>Click count: {count}{" "}
           <button onClick={() => setCount(count + 1)}>+1</button>
         </div>
