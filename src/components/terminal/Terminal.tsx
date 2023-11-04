7c2166dc26096cb4830da9255f3aedf202c27aa3
Jamil ETTEL <jamil.ettel@epitech.eu>
2023-11-04 18:09:14 +0100

    :construction: Working on adding pages

---
+++ src/components/terminal/Terminal.tsx
@@ -0,0 +1,5 @@
+import "./Terminal.scss";
+
+export default function Terminal() {
+  return <div className="terminal"></div>;
+}
--- src/modules/home/Home.tsx
+++ src/modules/home/Home.tsx
@@ -4,6 +4,7 @@ import AnimatedLink from "@/components/link/AnimatedLink";
 import { useState } from "react";
 import "./Home.scss";
 import { rubikMonoOne } from "@/utils/fonts";
+import Terminal from "@/components/terminal/Terminal";
 
 export default function Home() {
   const [count, setCount] = useState(0);
@@ -12,7 +13,9 @@ export default function Home() {
     <AnimatedContainer className="main-content">
       <div className="content">
         <h1 className={`${rubikMonoOne.className} main-title`}>JAMIL_ETTEL</h1>
-        <p>Welcome to my portfolio! I am Jamil, a fullstack developer.</p>
+        <h2 className="subtitle">A Full Stack developer</h2>
+
+        {/* <p>Welcome to my portfolio! I am Jamil, a fullstack developer.</p>
         <p>
           Now this website is still in development, but in the meantime you can
           checkout my <a href="https://www.github.com/jamilettel">Github</a> (my
@@ -27,8 +30,18 @@ export default function Home() {
         <div>
           Click count: {count}{" "}
           <button onClick={() => setCount(count + 1)}>+1</button>
+        </div> */}
+        <div className="links">
+          Choose a page...
+          <br />
+          <AnimatedLink href="/work">{"> my_work"}</AnimatedLink>
+          <br />
+          <AnimatedLink href="/contact">{"> contact"}</AnimatedLink>
+          <br />
+          <AnimatedLink href="/about">{"> about"}</AnimatedLink>
         </div>
       </div>
+      <Terminal />
     </AnimatedContainer>
   );
 }
