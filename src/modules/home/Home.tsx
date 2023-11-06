5a49e99986a85db00792628f0805cb1d742151d6
Jamil ETTEL <jamil.ettel@epitech.eu>
2023-11-06 14:52:09 +0100

    :dizzy: :construction: Transition animation WIP

--- src/modules/home/Home.tsx
+++ src/modules/home/Home.tsx
@@ -4,13 +4,56 @@ import AnimatedLink from "@/components/link/AnimatedLink";
 import Terminal from "@/components/terminal/Terminal";
 import { rubikMonoOne } from "@/utils/fonts";
 import "./Home.scss";
+import "./HomeTitle.scss";
+
+function OtherPageTitle({
+  className,
+  title,
+}: {
+  className: string;
+  title: string;
+}) {
+  return (
+    <div className={className}>
+      {title.split("").map((letter, i) => (
+        <div key={i}>{letter}</div>
+      ))}
+    </div>
+  );
+}
+
+function HomePageTitle() {
+  return (
+    <div>
+      <div className="title-wrapper">
+        <h1 className={`${rubikMonoOne.className} main-title`}>Jamil Ettel</h1>
+        <h1 className={`${rubikMonoOne.className} main-title-animated`}>
+          Jamil Ettel
+        </h1>
+        <OtherPageTitle
+          title="WORK"
+          className={`${rubikMonoOne.className} transition-work-title`}
+        />
+        <OtherPageTitle
+          title="CONTACT"
+          className={`${rubikMonoOne.className} transition-contact-title`}
+        />
+        <OtherPageTitle
+          title="ABOUT"
+          className={`${rubikMonoOne.className} transition-about-title`}
+        />
+        <div className="title-underscore" />
+      </div>
+      <h2 className="subtitle">A Full Stack developer</h2>
+    </div>
+  );
+}
 
 export default function Home() {
   return (
-    <AnimatedContainer className="main-content">
+    <AnimatedContainer className="main-content animate-out-work">
       <div className="content">
-        <h1 className={`${rubikMonoOne.className} main-title`}>JAMIL_ETTEL</h1>
-        <h2 className="subtitle">A Full Stack developer</h2>
+        <HomePageTitle />
         <div className="links">
           Choose a page...
           <br />
