ce426dbed7a2c9872af893e70417edb7989107fe
Jamil ETTEL <jamil.ettel@epitech.eu>
2023-11-09 18:14:36 +0100

    :sparkles: Added favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,64 @@ import { mono } from "@/utils/fonts";
 
 export const metadata: Metadata = {
   title: "Jamil Ettel",
-  description: "My portfolio",
+  description:
+    "Hello! I'm a fullstack developer, and this is my portfolio website!",
+  themeColor: "#191919",
+  icons: [
+    { rel: "apple-touch-icon", sizes: "57x57", url: "/apple-icon-57x57.png" },
+    { rel: "apple-touch-icon", sizes: "60x60", url: "/apple-icon-60x60.png" },
+    { rel: "apple-touch-icon", sizes: "72x72", url: "/apple-icon-72x72.png" },
+    { rel: "apple-touch-icon", sizes: "76x76", url: "/apple-icon-76x76.png" },
+    {
+      rel: "apple-touch-icon",
+      sizes: "114x114",
+      url: "/apple-icon-114x114.png",
+    },
+    {
+      rel: "apple-touch-icon",
+      sizes: "120x120",
+      url: "/apple-icon-120x120.png",
+    },
+    {
+      rel: "apple-touch-icon",
+      sizes: "144x144",
+      url: "/apple-icon-144x144.png",
+    },
+    {
+      rel: "apple-touch-icon",
+      sizes: "152x152",
+      url: "/apple-icon-152x152.png",
+    },
+    {
+      rel: "apple-touch-icon",
+      sizes: "180x180",
+      url: "/apple-icon-180x180.png",
+    },
+    {
+      rel: "icon",
+      type: "image/png",
+      sizes: "192x192",
+      url: "/android-icon-192x192.png",
+    },
+    {
+      rel: "icon",
+      type: "image/png",
+      sizes: "32x32",
+      url: "/favicon-32x32.png",
+    },
+    {
+      rel: "icon",
+      type: "image/png",
+      sizes: "96x96",
+      url: "/favicon-96x96.png",
+    },
+    {
+      rel: "icon",
+      type: "image/png",
+      sizes: "16x16",
+      url: "/favicon-16x16.png",
+    },
+  ],
 };
 
 export default function RootLayout({
