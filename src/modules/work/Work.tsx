69dff6ff19d8afc0e09564553a4563a0b5d2e20d
Jamil ETTEL <jamil.ettel@epitech.eu>
2023-11-09 17:32:19 +0100

    :speech_balloon: Adding Olympus to Work

--- src/modules/work/Work.tsx
+++ src/modules/work/Work.tsx
@@ -89,6 +89,8 @@ function MinistudioProject() {
             • Chat interface
             <br />• Webtoon viewing interface
           </p>
+          <h3>INVOLVMENT:</h3>
+          <p>2023</p>
 
           <Link href="https://ministudio.ai" target="_blank">
             VISIT WEBSITE ↗
@@ -96,6 +98,13 @@ function MinistudioProject() {
         </div>
         <div className="project-description">
           <h3>DESCRIPTION</h3>
+          <p>
+            Ministudio is a web application that allows children to create
+            magical characters. Children will upload a drawing to the app, or
+            use the integrated drawing tool. This drawing will then be used to
+            generate an image using AI. They can then chat with them, or create
+            pre-configured stories with them.
+          </p>
         </div>
       </div>
     </>
@@ -120,6 +129,8 @@ function NorelaProject() {
             <br />• Payment & Quotes
             <br />• Database design & frontend implementation
           </p>
+          <h3>INVOLVMENT:</h3>
+          <p>2023</p>
 
           <Link href="https://app.e-norela.com/" target="_blank">
             VISIT WEBSITE ↗
@@ -127,6 +138,52 @@ function NorelaProject() {
         </div>
         <div className="project-description">
           <h3>DESCRIPTION</h3>
+          <p>
+            E-norela is an event staff booking platform. It suggests a staff
+            configuration for your event based on an onboarding questionnaire.
+            Your are then able to change the configuration based on your needs.
+          </p>
+        </div>
+      </div>
+    </>
+  );
+}
+
+function OlympusProject() {
+  return (
+    <>
+      <h2>Olympus - UNRELEASED</h2>
+      <div className="project">
+        <PixelatedImage src="/olympus.png" />
+        <div className="project-overview">
+          <h3>RESPONSIBILITIES:</h3>
+          <p>Full stack developer / Devops / </p>
+          <h3>TECH STACK:</h3>
+          <p>
+            React-Native / Django / Hasura / PostgreSQL <br />
+            AWS / Terraform
+          </p>
+          <h3>MAIN WORK:</h3>
+          <p>
+            • Main developer for the project
+            <br />• Database design
+            <br />• Brainstorming & User testing
+          </p>
+          <h3>INVOLVMENT:</h3>
+          <p>2023</p>
+        </div>
+        <div className="project-description">
+          <h3>DESCRIPTION</h3>
+          <p>
+            Olympus is an educational application for high school mathematics.
+            {" It's"} a gamified learning application that prompts students to
+            practice a bit of math every day. The goal is to make math fun and
+            simple.
+            <br />
+            As I stopped working on this project, we were in the process of
+            adding user statistics and new exercise types to encourage the users
+            to practice more.
+          </p>
         </div>
       </div>
     </>
@@ -148,7 +205,7 @@ export default function About() {
         <PortfolioProject />
         <MinistudioProject />
         <NorelaProject />
-        <h2>Olympus - UNRELEASED</h2>
+        <OlympusProject />
         <h2>{"Graines d'ici"}</h2>
       </div>
     </Layout>
